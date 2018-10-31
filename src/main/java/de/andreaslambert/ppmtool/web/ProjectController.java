ea47608d7dacb916f404e6c148086084ae1b7890
al <alambert@gmx.de>
2018-10-31 11:01:48 +0100

    Setup basic web service with validation - before refacgtoring

--- src/main/java/de/andreaslambert/ppmtool/web/ProjectController.java
+++ src/main/java/de/andreaslambert/ppmtool/web/ProjectController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/project")
@@ -23,7 +26,9 @@ public class ProjectController {
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult) {
         if (bindingResult.hasErrors()){
-            return new ResponseEntity<String>("Invalid object", HttpStatus.BAD_REQUEST);
+            Map<String, String> fieldErrorMap = bindingResult.getFieldErrors().stream()
+                    .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+            return new ResponseEntity<>( fieldErrorMap, HttpStatus.BAD_REQUEST);
         }
         Project persistentProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(persistentProject, HttpStatus.CREATED);
