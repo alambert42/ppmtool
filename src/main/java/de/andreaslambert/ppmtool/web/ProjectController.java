695dd11585708ae0c5e7c1b28ab019f94871809d
al <alambert@gmx.de>
2018-10-31 11:07:12 +0100

    extract error validation refactoring

--- src/main/java/de/andreaslambert/ppmtool/web/ProjectController.java
+++ src/main/java/de/andreaslambert/ppmtool/web/ProjectController.java
@@ -1,20 +1,18 @@
 package de.andreaslambert.ppmtool.web;
 
 import de.andreaslambert.ppmtool.domain.Project;
+import de.andreaslambert.ppmtool.services.MapValidationErrorService;
 import de.andreaslambert.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/project")
@@ -23,14 +21,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()){
-            Map<String, String> fieldErrorMap = bindingResult.getFieldErrors().stream()
-                    .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
-            return new ResponseEntity<>( fieldErrorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> fieldErrorMap = mapValidationErrorService.mapValidationService(bindingResult);
+        if (fieldErrorMap != null) return fieldErrorMap;
+
         Project persistentProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(persistentProject, HttpStatus.CREATED);
     }
+
 }
